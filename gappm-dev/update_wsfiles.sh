14a2bbf079e6d61b4ee5a9278f1edd9045370327
Gabriel Ochsenhofer <gabs@gabs.tv>
2014-05-05 16:11:08 -0300

    File gen

---
+++ gappm-dev/update_wsfiles.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+gobytes -var=V_index_html -pack=webfiles ./../webclient/index.html > ./../gappm/webfiles/index.html.go
+gobytes -var=V_main_css -pack=webfiles ./../webclient/main.css > ./../gappm/webfiles/main.css.go
+gobytes -var=V_main_js -pack=webfiles ./../webclient/main.js > ./../gappm/webfiles/main.js.go
+gobytes -var=V_pshubcl_js -pack=webfiles ./../webclient/pshubcl.js > ./../gappm/webfiles/pshubcl.js.go
